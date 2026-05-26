8bb741f2087d54f50a1ad16f0970801d805aa812
Sam Joyce <42083561+sammyjoyce@users.noreply.github.com>
2026-05-26 22:56:31 +1000

    fix: use windows binary suffix in smoke test

--- test/main.zig
+++ test/main.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
 const testing = std.testing;
+const builtin = @import("builtin");
 
 test "installed binary starts" {
     const allocator = testing.allocator;
-    const binary_path = "./zig-out/bin/myapp";
+    const binary_path = if (builtin.os.tag == .windows)
+        "./zig-out/bin/myapp.exe"
+    else
+        "./zig-out/bin/myapp";
 
     const file = try std.Io.Dir.cwd().openFile(testing.io, binary_path, .{});
     file.close(testing.io);
